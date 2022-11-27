6fc268282b4891640925c77ecbb9f322c3516b34
Mohmmed <m7md206000@gmail.com>
2022-11-27 18:00:03 +0300

    initial commit

---
+++ learn.js
@@ -0,0 +1,31 @@
+document.getElementsByClassName("main-list");
+
+// const header = document.querySelector("ain-list");
+// header.remove()
+// document.getElementsByClassName(".main-list")
+
+// const headerr = document.querySelector(".main-list")
+// headerr.remove()
+wndow.alert(" Welcome يا قوي ");
+
+// remove 
+
+// const e = document.getElementsByTagName("h1")
+// e.remove()
+
+// create emelemnt
+
+function myFunction() {
+  document.getElementById("myh1").setAttribute("class", "demo"); 
+  document.getElementById("myh2").setAttribute("id" , "Hi" )
+  document.getElementById("quiz").setAttribute("id" , "quizjs")
+  document.getElementById("bhide").setAttribute("id" , "btt")
+  document.getElementById("h1anime").setAttribute("id" , "h1move")
+  document.getElementById("hideme").setAttribute("id" , "hidemejs")
+
+};
+
+
+
+
+
